fd1c7231ed3b1c329948be2b7cf0ef154f62311b
Akshar Sistu <akshar.sistu@gmail.com>
2023-06-30 21:26:26 +0530

    Created backend part for modifying substages

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import 'react-app-polyfill/ie9'
 
-const BASE_URL = 'http://192.168.169.70:8000'
+const BASE_URL = 'http://10.4.19.2:8000'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
